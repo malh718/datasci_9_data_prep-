63c37c612930253ed1a04598286f385fd9144b51
maliha <maliha.718@gmail.com>
2023-11-22 01:26:00 +0000

    data and model_dev

---
+++ model_dev/scripts/p1_extract.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+
+## get data 
+
+# original link: lake county cancer rates 
+# data download link: 
+datalink = 'https://data-lakecountyil.opendata.arcgis.com/datasets/lakecountyil::cancer-rates.csv?where=1=1&outSR=%7B%22latestWkid%22%3A3435%2C%22wkid%22%3A102671%7D'
+
+df = pd.read_csv(datalink)
+df.size
+df.sample(5)
+
+
+
+
+
+## save as csv to WK9/code/model_dev/data/raw
+df.to_csv('model_dev/data/processed/raw/cancer_data.csv', index=False)
+
+## save as pickle to WK9/code/model_dev/data/raw
+df.to_pickle('model_dev/data/processed/raw/cancer_data.pkl')
+
+
+# Lake county cancer rates  
+## original link: https://catalog.data.gov/dataset/cancer-rates-5cf0c
---
+++ model_dev/scripts/p2_transform.py
@@ -0,0 +1,78 @@
+import pandas as pd 
+from sklearn.preprocessing import OrdinalEncoder
+
+## get data raw
+df = pd.read_pickle('model_dev/processed/raw/cancer_data.pkl')
+
+## get reporting_distrcts
+df_rpt_dist = pd.read_csv('model_dev/processed/raw/Birth_Statistics.csv')
+## get column names
+df.columns
+
+## do some data cleaning of colun names, 
+## make them all lower case, replmove white spaces and rpelace with _ 
+df.columns = df.columns.str.lower().str.replace(' ', '_')
+df.columns
+
+df_rpt_dist.columns = df_rpt_dist.columns.str.lower().str.replace(' ', '_')
+df_rpt_dist.columns
+
+## get data types
+df.dtypes # nice combination of numbers and strings/objects 
+len(df)
+
+df.sample(5)
+
+## drop columns
+to_drop = [
+    'fid',
+    'shape_area',
+    'shape_length'
+]
+
+df.drop(to_drop, axis=1, inplace=True, errors='ignore')
+
+
+df.sample(5)
+
+df.columns
+
+# keep columns 
+to_keep = [
+    'rd',
+    'name'
+]
+df_rpt_dist = df_rpt_dist[to_keep]
+df_rpt_dist['rd'] = pd.to_numeric(df_rpt_dist['rd'], errors='coerce')
+df_rpt_dist.dropna(inplace=True)
+df_rpt_dist['rd'] = df_rpt_dist['rd'].astype('int64')
+
+
+
+
+
+## perform ordinal encoding on date_occ
+enc = OrdinalEncoder()
+enc.fit(df[['all_cancer']])
+df['all_cancer_ordinal'] = enc.transform(df[['all_cancer']])
+
+## create dataframe with mapping
+df_mapping_date = pd.DataFrame(enc.categories_[0], columns=[zip])
+df_mapping_date['all_cancer_ordinal'] = df_mapping_date.index
+df_mapping_date
+
+## save mapping to csv
+df_mapping_date.to_csv('model_dev/PROCESSEDDATA/mapping_date.csv', index=False)
+
+
+## create dataframe with mapping
+df_mapping_area = pd.DataFrame(enc.categories_[0], columns=['area_name'])
+df_mapping_area['area_name_ordinal'] = df_mapping_area.index
+df_mapping_area.head(5)
+# save mapping to csv
+df_mapping_area.to_csv('WK9/code/model_dev/data/processed/mapping_area.csv', index=False)
+
+
+
+
+
